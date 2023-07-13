3a0ac0f3b524501376182247773c3a5cfa5b97f9
yunsteel <archer0082@naver.com>
2023-07-13 14:38:27 +0900

    dev: add padding div

---
+++ src/components/common/PaddingDiv.tsx
@@ -0,0 +1,16 @@
+import { FC, HTMLAttributes, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+const PaddingDiv: FC<PropsWithChildren<HTMLAttributes<HTMLDivElement>>> = ({
+  children,
+  className,
+  ...props
+}) => {
+  return (
+    <div {...props} className={twMerge("px-8 tablet:px-[100px]", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default PaddingDiv;
