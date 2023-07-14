f3c92b3e9c74604b2aa2f2a06193cb8bb6b8fa5f
yunsteel <archer0082@naver.com>
2023-07-14 18:30:16 +0900

    style: scinapse

--- src/components/home/Sections/Section5.tsx
+++ src/components/home/Sections/Section5.tsx
@@ -7,7 +7,7 @@ import OurPartners from "../Partners";
 
 const Cards: FC = () => {
   return (
-    <div className="tablet:max-h-[500px] p-12 flex tablet:gap-y-0 gap-y-10 tablet:flex-row flex-col overflow-hidden items-center relative tablet:gap-x-8 bg-pluto-deep-blue rounded-3xl text-white tablet:items-center">
+    <div className="tablet:h-[500px] h-[850px] p-12 flex tablet:gap-y-0 gap-y-10 tablet:flex-row flex-col overflow-hidden items-center relative tablet:gap-x-8 bg-pluto-deep-blue rounded-3xl text-white tablet:items-center">
       <div className="tablet:w-2/5 space-y-8">
         <div className="text-3xl">Scinapse</div>
         <div className="font-extrabold text-3xl">
@@ -21,7 +21,7 @@ const Cards: FC = () => {
         </div>
         <ContactUsButton className="bg-white text-black" />
       </div>
-      <div className="tablet:w-[556px] h-[389px] tablet:translate-x-1/4 relative overflow-hidden rounded-3xl bg-white w-full">
+      <div className="tablet:w-[556px] h-[389px] tablet:translate-x-1/4 tablet:relative overflow-hidden rounded-3xl bg-white w-full absolute bottom-0 rounded-t-none tablet:rounded-t-3xl">
         <Image
           src="https://framerusercontent.com/images/jIoRwB41bckHO8BWLN0CJwa0.png"
           className="bg-no-repeat absolute inset-0 object-cover object-center w-full h-full"
@@ -30,6 +30,15 @@ const Cards: FC = () => {
           height={389}
         />
       </div>
+      {/* <div className="tablet:w-[556px] h-[389px] tablet:translate-x-1/4 relative overflow-hidden rounded-3xl bg-white w-full">
+        <Image
+          src="https://framerusercontent.com/images/jIoRwB41bckHO8BWLN0CJwa0.png"
+          className="bg-no-repeat absolute inset-0 object-cover object-center w-full h-full"
+          alt="Scinapse main page"
+          width={556}
+          height={389}
+        />
+      </div> */}
     </div>
   );
 };
