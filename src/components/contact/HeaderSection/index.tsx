00a0ea45d45031fa7dc213a9fa3cb883a4abb351
yunsteel <archer0082@naver.com>
2023-07-17 18:09:46 +0900

    dev: attach animation to contact page

--- src/components/contact/HeaderSection/index.tsx
+++ src/components/contact/HeaderSection/index.tsx
@@ -1,20 +1,43 @@
-import { FC } from "react";
-import PaddingDiv from "../../common/PaddingDiv";
+import { useSequenceAnimation } from "@/components/hooks/useSequenceAnimation";
+import { FC, useEffect, useRef } from "react";
 
 interface Props {}
 
 const HeaderSection: FC<Props> = () => {
+  const animate = useSequenceAnimation();
+
+  const connectRef = useRef(null);
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+
+  useEffect(() => {
+    animate([
+      { ref: titleRef },
+      { ref: connectRef, withNav: true, option: { at: 0.5 } },
+      { ref: descriptionRef, withNav: true, option: { at: 0.5 } },
+    ]);
+  });
+
   return (
     <div className="flex-1 space-y-6">
-      <div className="font-semibold text-[#f2f6fd] text-opacity-30 tracking-widest">
+      <div
+        className="font-semibold text-[#f2f6fd] text-opacity-30 tracking-widest"
+        ref={connectRef}
+      >
         {"LET'S CONNECT"}
       </div>
 
-      <div className="text-3xl tablet:text-5xl font-bold text-white">
+      <div
+        className="text-3xl tablet:text-5xl font-bold text-white"
+        ref={titleRef}
+      >
         Contact us
       </div>
 
-      <div className="text-lg text-white font-medium leading-relaxed">
+      <div
+        className="text-lg text-white font-medium leading-relaxed"
+        ref={descriptionRef}
+      >
         Discover new insights for better research and business. We focus on
         in-depth research data and analytics from research papers.
       </div>
--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
-import HeaderSection from "./HeaderSection";
-import ContactForm from "./ContactForm";
 import PaddingDiv from "../common/PaddingDiv";
+import ContactForm from "./ContactForm";
+import HeaderSection from "./HeaderSection";
 
 interface Props {}
 
