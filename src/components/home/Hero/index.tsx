3d27535e11cb302f34bd1a8f710b7c55d0e1fc35
yunsteel <archer0082@naver.com>
2023-07-17 14:34:55 +0900

    resolve merge conflicts

--- src/components/home/Hero/index.tsx
+++ src/components/home/Hero/index.tsx
@@ -1,4 +1,5 @@
 import MaxWidth from "@/components/common/MaxWidth";
+import { Motion } from "@/pages";
 import { FC } from "react";
 import ScrollToDiscover from "../ScrollToDiscover";
 import Lottie from "lottie-react";
@@ -6,16 +7,20 @@ import * as lottieData from "../../../../public/lottie.json";
 
 const HomeHero: FC = () => {
   return (
-    <div className="h-[800px] relative flex tablet:items-center w-full overflow-hidden">
-      <MaxWidth className="space-y-12 py-16 tablet:py-48 min-h-0">
-        <div className="break-words font-extrabold text-3xl tablet:text-5xl">
-          <div>Innovate</div>
-          <div>Innovations</div>
-        </div>
-        <div className="tablet:w-1/2 tablet:text-xl">
-          Discover new insights for better research and business.
+    <div className="py-16 tablet:py-48 h-[800px] flex tablet:items-center">
+      <MaxWidth className="space-y-12">
+        <Motion className="break-words font-extrabold text-3xl tablet:text-5xl !leading-[1.3]">
+          Innovate
           <br />
-          We focus on in-depth research data and analytics from research papers.
+          Innovations
+        </Motion>
+        <div className="tablet:w-1/2 tablet:text-xl">
+          <Motion>
+            Discover new insights for better research and business.
+            <br />
+            We focus on in-depth research data and analytics from research
+            papers.
+          </Motion>
         </div>
         <ScrollToDiscover />
       </MaxWidth>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,41 @@ import HomeSection4 from "@/components/home/Sections/Section4";
 import HomeSection5 from "@/components/home/Sections/Section5";
 import HomeSection6 from "@/components/home/Sections/Section6";
 import Head from "next/head";
+import { motion, useAnimate } from "framer-motion";
+import { FC, PropsWithChildren, useEffect } from "react";
+
+export const Motion: FC<PropsWithChildren<{ className?: string }>> = ({
+  children,
+  className,
+}) => {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    const animation = async () => {
+      await animate(scope.current, { opacity: 0, marginTop: 100 });
+      await animate(scope.current, { opacity: 1, marginTop: 0 });
+    };
+
+    animation();
+  }, [animate, scope]);
+
+  return (
+    <div ref={scope} className={className}>
+      {children}
+    </div>
+  );
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, marginTop: 100 }}
+      whileInView={{ opacity: 1, marginTop: 0 }}
+      viewport={{ once: true }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
 
 export default function Home() {
   return (
