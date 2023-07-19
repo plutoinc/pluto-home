ca7226811ab0eecb3d3d2362cbe6fd3241f0dbb9
cksdnr3 <cksdnr3333@gmail.com>
2023-07-19 13:59:47 +0900

    chore: fix some styles

--- src/components/products/AboutSection/index.tsx
+++ src/components/products/AboutSection/index.tsx
@@ -1,24 +1,22 @@
-import ScrollMotionInView from "@/components/common/Animation/ScrollMotionInView";
-import PaddingDiv from "@/components/common/PaddingDiv";
-import { ABOUT_PATH } from "@/routes";
-import Link from "next/link";
-import { FC } from "react";
-import Disclosure from "../../common/Disclosure";
+import ScrollMotionInView from '@/components/common/Animation/ScrollMotionInView';
+import PaddingDiv from '@/components/common/PaddingDiv';
+import { ABOUT_PATH } from '@/routes';
+import Link from 'next/link';
+import { FC } from 'react';
+import Disclosure from '../../common/Disclosure';
 
 interface Props {}
 
 const AboutSection: FC<Props> = () => {
   return (
-    <div className="bg-[#f4f9ff] mx-auto">
-      <PaddingDiv className="py-16 space-y-8 mx-auto max-w-7xl tablet:flex tablet:space-x-16 tablet:space-y-0">
-        <ScrollMotionInView className="space-y-8 max-w-xs">
-          <div className="text-2xl tablet:text-3xl font-bold">
-            Unlock powerful insights with our technology
-          </div>
+    <div className="mx-auto bg-[#f4f9ff]">
+      <PaddingDiv className="mx-auto max-w-7xl space-y-8 py-16 tablet:flex tablet:space-x-16 tablet:space-y-0">
+        <ScrollMotionInView className="max-w-xs space-y-8">
+          <div className="text-2xl font-bold tablet:text-3xl">Unlock powerful insights with our technology</div>
 
           <Link
             href={{ pathname: ABOUT_PATH }}
-            className="px-4 py-2.5 inline-block bg-black rounded-lg text-white text-sm font-bold shadow-gray-300 shadow-lg"
+            className="inline-block rounded-lg bg-gray-900 px-4 py-2.5 text-sm font-bold text-white shadow-md shadow-gray-400"
           >
             About us
           </Link>
@@ -29,10 +27,10 @@ const AboutSection: FC<Props> = () => {
             type="list"
             title="Gain new insight"
             descriptions={[
-              "Research funding trends and relevant information.",
-              "Extraction of experimental data and integration of data from papers.",
-              "Search for patents and relevant firms.",
-              "Back your cases with integrated experimental results, funding information, patents, enterprise research, and more.",
+              'Research funding trends and relevant information.',
+              'Extraction of experimental data and integration of data from papers.',
+              'Search for patents and relevant firms.',
+              'Back your cases with integrated experimental results, funding information, patents, enterprise research, and more.',
             ]}
           />
 
@@ -41,9 +39,9 @@ const AboutSection: FC<Props> = () => {
             title="Cutting-edge research trends"
             description="Get quick access to the latest research trends we identify:"
             descriptions={[
-              "Find rising stars in specific fields",
-              "Technology commercialization stages in subunits of research fields.",
-              "Detailed research of subgroup areas.",
+              'Find rising stars in specific fields',
+              'Technology commercialization stages in subunits of research fields.',
+              'Detailed research of subgroup areas.',
             ]}
           />
 
@@ -54,9 +52,9 @@ const AboutSection: FC<Props> = () => {
             descriptions={[
               "Each author's research performance analysis on a yearly basis.",
               "Identify each author's specific research field and their impact on the field.",
-              "Patents and business enterprises related to each author are also provided.",
-              "Research performance analysis of each research institution.",
-              "Comparative analysis across institutions.",
+              'Patents and business enterprises related to each author are also provided.',
+              'Research performance analysis of each research institution.',
+              'Comparative analysis across institutions.',
             ]}
           />
         </ScrollMotionInView>
