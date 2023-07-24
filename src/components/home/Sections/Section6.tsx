98746b72462bf9e23b5026e4f81b99584d5cb0ba
yunsteel <archer0082@naver.com>
2023-07-24 23:39:28 +0900

    dev: add playback rate

--- src/components/home/Sections/Section6.tsx
+++ src/components/home/Sections/Section6.tsx
@@ -1,11 +1,19 @@
 import ScrollMotionInView from '@/components/common/Animation/ScrollMotionInView';
 import MaxWidth from '@/components/common/MaxWidth';
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import ContactUsLink from '@/components/common/ContactUsLink';
 
 interface Props {}
 
 const HomeSection6: FC<Props> = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 2;
+    }
+  }, []);
+
   return (
     <div className="bg-[#F0F6FF]">
       <MaxWidth className="space-y-8 py-16">
@@ -25,7 +33,7 @@ const HomeSection6: FC<Props> = () => {
         </ScrollMotionInView>
         <ContactUsLink className="inline-block rounded-lg bg-pluto-dark-blue px-4 py-2.5 text-sm font-bold text-white shadow-md shadow-gray-400" />
         <div className="rounded-2xl bg-white p-4">
-          <video src="/assets/home/renewable_energy.mov" autoPlay muted />
+          <video src="/assets/home/renewable_energy.mov" autoPlay muted loop ref={videoRef} />
         </div>
       </MaxWidth>
     </div>
