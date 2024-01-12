336ffb8fa027b89505cde6df31810dc492db08b7
yunsteel <archer0082@naver.com>
2024-01-12 16:52:34 +0900

    dev: add og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
-import Head from "next/head";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  weight: ["400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['400', '500', '600', '700', '800'],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Pluto</title>
+        <title>Pluto Labs</title>
+        <meta property="og:title" content="Pluto Labs" />
+        <meta property="og:url" content="https://www.pluto.im/" />
+        <meta property="og:image" content="https://assets.scinapse.io/pluto-og-image.png" />
       </Head>
 
       <main className={`${inter.variable} font-sans`}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,20 @@
-import Footer from "@/components/common/Footer";
-import Header from "@/components/common/Header";
-import HomeHero from "@/components/home/Hero";
-import BlogSection from "@/components/home/Sections/Blog";
-import HomeSection1 from "@/components/home/Sections/Section1";
-import HomeSection2 from "@/components/home/Sections/Section2";
-import HomeSection3 from "@/components/home/Sections/Section3";
-import HomeSection4 from "@/components/home/Sections/Section4";
-import HomeSection5 from "@/components/home/Sections/Section5";
-import HomeSection6 from "@/components/home/Sections/Section6";
-import Head from "next/head";
+import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+import HomeHero from '@/components/home/Hero';
+import BlogSection from '@/components/home/Sections/Blog';
+import HomeSection1 from '@/components/home/Sections/Section1';
+import HomeSection2 from '@/components/home/Sections/Section2';
+import HomeSection3 from '@/components/home/Sections/Section3';
+import HomeSection4 from '@/components/home/Sections/Section4';
+import HomeSection5 from '@/components/home/Sections/Section5';
+import HomeSection6 from '@/components/home/Sections/Section6';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Pluto Labs</title>
-      </Head>
-
       <Header />
 
-      <div className="text-gray-700 bg-[#f4f9ff]">
+      <div className="bg-[#f4f9ff] text-gray-700">
         <HomeHero />
         <HomeSection1 />
         <HomeSection2 />
